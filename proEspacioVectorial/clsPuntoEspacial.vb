be60aaa1fbe3f71541885e94bf8b56c220258a37
Miguel Quinteiro <maq777@gmail.com>
2023-05-13 12:28:20 -0300

    Inicio de Proyecto proEspacioVectorial

---
+++ proEspacioVectorial/clsPuntoEspacial.vb
@@ -0,0 +1,70 @@
+﻿
+Public Class clsPuntoEspacial
+#Region "AREA DE DATOS"
+    '*******************************************************************
+    ' Declaracion de los Atributos privados para encapsulamiento
+    Private aCoordenadaX As Integer
+    Private aCoordenadaY As Integer
+    Private aCoordenadaZ As Integer
+
+    ' Metodos Get y Set para manipulacion de datos de la clase
+    ' CoordenadaX
+    '*********************************************************
+    Public Property CoordenadaX As Integer
+        Get
+            CoordenadaX = aCoordenadaX
+        End Get
+        Set(ByVal value As Integer)
+            aCoordenadaX = value
+        End Set
+    End Property
+    ' CoordenadaY
+    '*********************************************************
+    Public Property CoordenadaY As Integer
+        Get
+            CoordenadaY = aCoordenadaY
+        End Get
+        Set(ByVal value As Integer)
+            aCoordenadaY = value
+        End Set
+    End Property
+    ' CoordenadaZ
+    '*********************************************************
+    Public Property CoordenadaZ As Integer
+        Get
+            CoordenadaZ = aCoordenadaZ
+        End Get
+        Set(ByVal value As Integer)
+            aCoordenadaZ = value
+        End Set
+    End Property
+#End Region
+
+#Region "AREA DE METODOS Y FUNCIONALIDAD"
+    '*******************************************************************
+    ' Metodo constructor y destructor de la clase
+    ' Constructor
+    Public Sub New()
+        ' Que hacer al instanciar el objeto
+        aCoordenadaX = 0
+        aCoordenadaY = 0
+        aCoordenadaZ = 0
+    End Sub
+    ' Constructor
+    Public Sub New(ByVal x As Integer, ByVal y As Integer, ByVal z As Integer)
+        ' Que hacer al instanciar el objeto
+        aCoordenadaX = x
+        aCoordenadaY = y
+        aCoordenadaZ = z
+    End Sub
+    ' Destructor
+
+    ' Metodos publicos para la funcionalidad de la clase
+    ' Distancia Origen
+    Public Function DistanciaOrigen() As Double
+        DistanciaOrigen = Math.Sqrt((aCoordenadaX ^ 2) + (aCoordenadaY ^ 2) + (aCoordenadaZ ^ 2))
+    End Function
+
+#End Region
+
+End Class
---
+++ proEspacioVectorial/frmEspacioVectorial.vb
@@ -0,0 +1,42 @@
+﻿
+
+Public Class frmEspacioVectorial
+    ' Declaración de variables
+    Dim miGrafico As Graphics
+
+    ' AL CARGAR EL FORMULARIO
+    Private Sub frmEspacioVectorial_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+    End Sub
+
+    ' Procedimiento Limpiar
+    Private Sub Limpiar()
+    End Sub
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        miGrafico = Me.CreateGraphics
+        miGrafico.Clear(Color.Gray)
+
+
+        ' Create pen.
+        Dim blackPen As New Pen(Color.Black, 3)
+
+        'al punto central del área cliente del formulario
+        'Aquí movemos el eje de coordenadas
+        'al punto central del área cliente del formulario
+        miGrafico.TranslateTransform(Me.ClientSize.Width / 2, Me.ClientSize.Height / 2)
+
+
+        miGrafico.DrawLine(blackPen, 0, -200, 0, 200)
+        miGrafico.DrawLine(blackPen, -200, 0, 200, 0)
+
+
+        miGrafico.DrawLine(blackPen, CInt(-1600 * 0.07071), CInt(800 * 0.07071), CInt(150 * 0.7071), CInt(-800 * 0.07071))
+
+
+        ' Declaración de variables
+        Dim oPunto As New clsPuntoEspacial(0, 0, 0)
+
+        miGrafico.FillEllipse(Brushes.Blue, oPunto.CoordenadaX, oPunto.CoordenadaY, 5, 5)
+    End Sub
+End Class
